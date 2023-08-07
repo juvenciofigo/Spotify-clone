77d12111ce2f1a7d1759280fb04c831ef566f3c7
juvenciofigo <juvenciofigo@gmail.com>
2023-08-08 00:57:59 +0200

    s

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Spotify-clone
 
 <a href="https://juvenciofigo.github.io/Spotify-clone/src/index.html">Spotify-clone</a>
-
-Os ícones nao sao mostrados no github pages
--- src/script/script.js
+++ src/script/script.js
@@ -1,11 +1,8 @@
-import 'boxicons'
-
 const divmusi = document.querySelectorAll(".divmusic");
 const crol = document.querySelector("section");
 
 function meuEvento(event) {
-
-        event.target.children[0].classList.toggle("hidden");
+    event.target.children[0].classList.toggle("hidden");
 }
 
 divmusi.forEach(function (div) {
